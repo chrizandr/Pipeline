5933f572034a2b74ae45c4744861957ed856e817
santhoshini <santhoshini.g14@iits.in>
2016-04-10 16:04:54 +0530

    New test file a.asm

---
+++ a.asm
@@ -0,0 +1,7 @@
+addi   $gp, $zero, 1000   # putting base address 1000 into
+lw   $s2, 4($gp)               # loading variable b into $s2
+lw   $s3, 8($gp)               # loading variable c into $s3
+add $s1, $s2, $s3           # sum in $s1
+sw   $s1, $gp                  # storing sum into variable a
+addi $s1,$s1,4
+addi $s4,$s1,$s5
